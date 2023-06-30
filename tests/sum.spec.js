6877da12792fb15df668d5b4de9f379799806fd0
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-06-30 02:17:09 -0300

    Implementado teste para ver se a soma de 0 e 0 é 0

--- tests/sum.spec.js
+++ tests/sum.spec.js
@@ -3,12 +3,11 @@ const sum = require('../src/sum');
 describe('1 - Crie os casos de teste da funcão `Sum`', () => {
 
   it ('Testa se ao receber "4" e "5" como parâmetro, retorna "9" como resultado', () => {
-    expect(sum(4,5)).toBe(9);
+    expect(sum(4, 5)).toBe(9);
   });
 
   it ('Testa se ao receber "0" nos dois parâmetros, retorna o resultado "0"', () => {
-    // Remova o fail e escreva seus testes abaixo
-    fail('Teste vazio!');
+    expect(sum(0,0)).toBe(0);
   });
   
   it ('Testa se dispara um erro, caso receba como parâmetro uma string', () => {
